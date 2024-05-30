885003d59cca9d8284a4c9a78bff73c077892d89
Manny-Sandhu <140126737+Manny-Sandhu@users.noreply.github.com>
2024-05-29 19:06:20 -0700

    Update FPSInput.cs

--- ICS223_FPS/Assets/Scripts/FPSInput.cs
+++ ICS223_FPS/Assets/Scripts/FPSInput.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     [SerializeField] private float speed = 9.0f;
+    [SerializeField] private CharacterController charController;
     private float horizontalInput;
     private float verticalInput;
+    private float gravity = -9.8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,20 @@ public class FPSInput : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        // Clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+        // take speed into account
+        movement *= speed;
+        // add gravity
+        movement.y = gravity;
+        // make movement processor independent
+        movement *= Time.deltaTime;
+        // Convert local to global coordinates
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+
     }
 
     private void FixedUpdate()
